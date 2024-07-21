0ba993ddb8627cdc2d3335f63e65a9fa3cc9fd78
suleiman <suleimananwarmubarak@gmail.com>
2024-07-21 20:47:16 +0300

    the bullet on the landing section are working

--- main.js
+++ main.js
@@ -7,3 +7,20 @@ window.onscroll = function (e) {
         goUp.classList.remove("show");
     }
 };
+
+function changeColor(event) {
+    const listItems = document.querySelectorAll(".bullets li");
+    listItems.forEach((item) => {
+        item.classList.remove("active");
+    });
+    event.target.classList.add("active");
+}
+
+document.querySelectorAll(".bullets li").forEach((item) => {
+    item.addEventListener("click", changeColor);
+});
+
+function rightArrow(event) {
+    const rightArr = document.querySelector(".landing i");
+    console.log("working");
+}
